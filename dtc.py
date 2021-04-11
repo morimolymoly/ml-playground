db94b0f65c855d86c0d96c388e1219eb779a038f
morimolymoly <morimolymoly@gmail.com>
2021-04-11 15:53:37 +0900

    decision tree

---
+++ dtc.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+# アヤメの訓練データ
+iris = load_iris()
+
+x = iris.data[:,2:]
+y = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf.fit(x, y)
+
+from graphviz import Source
+from sklearn.tree import export_graphviz
+import os
+
+export_graphviz(
+    tree_clf,
+    out_file=os.path.join("iris_tree.dot"),
+    class_names=iris.target_names,
+    rounded=True,
+    filled=True
+)
